48d63fb5b27953559bdcc6c2775d9ddab7d8eb93
Jesus Reynoso <jesus.srg19@gmail.com>
2025-08-11 11:27:36 -0600

    Add friend to list

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function agregarAmigo() {
+    let nombre = document.getElementById("nombreAmigo").value;
+    if ((nombre.length) > 0) {
+        addFriend(nombre);
+    } else {
+        alert("Ingrese un nombre");
+    }
+}
+
+function addFriend(nombreAmigo) {
+    let friendList = document.getElementById("listaAmigos");
+    listaAmigos.push(nombreAmigo);
+    document.getElementById("nombreAmigo").value = "";
+    
+    let listItem = document.createElement("li");
+    listItem.innerHTML = nombreAmigo;
+    friendList.appendChild(listItem);
+}
+
+function setTextToElement(element, text) {
+    document.getElementById(element).innerHTML = text;
+}
+
+let listaAmigos = [];

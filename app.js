a0794e43801b41dfab694b6d067b37debf40c257
Jesus Reynoso <jesus.srg19@gmail.com>
2025-08-11 23:50:04 -0600

    Added pick random friend

--- app.js
+++ app.js
@@ -8,11 +8,23 @@ function agregarAmigo() {
     }
 }
 
+function sortearAmigo() {
+    if (listaAmigos.length > 0) {
+        let resultadoElement = document.getElementById("resultado");
+        let numeroRandom = Math.floor(Math.random() * listaAmigos.length);
+        console.log(numeroRandom);
+        let nombreResultado = listaAmigos[numeroRandom];
+        resultadoElement.innerHTML = "El nombre del amigo random es: " + nombreResultado;
+    } else {
+        alert("No hay amigos registrados :(");
+    }
+}
+
 function addFriend(nombreAmigo) {
     let friendList = document.getElementById("listaAmigos");
     listaAmigos.push(nombreAmigo);
     document.getElementById("nombreAmigo").value = "";
-    
+
     let listItem = document.createElement("li");
     listItem.innerHTML = nombreAmigo;
     friendList.appendChild(listItem);
